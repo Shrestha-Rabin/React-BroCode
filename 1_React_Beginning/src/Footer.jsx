b8ab6aa4f6e507c084236e309ebfb1181c75fdae
Rabin Shrestha <077bcsit028.rabin@scst.edu.np>
2025-02-20 16:39:48 +0545

    complete introduction

---
+++ 1_React_Beginning/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer () {
+
+    return (
+        <footer>
+            <p>@copy; {new Date().getFullYear()} Rabin Shrestha</p>
+        </footer>
+    ); 
+
+}
+
+export default Footer
