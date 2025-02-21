bcecb94e373dd7ead505632ef64884957f5e9ba8
Rabin Shrestha <077bcsit028.rabin@scst.edu.np>
2025-02-21 15:13:45 +0545

    props complete

---
+++ 4_Props/src/App.jsx
@@ -0,0 +1,15 @@
+import Student from "./Student"
+
+function App() {
+
+
+  return (
+    <>
+      <Student name="Rabin Shrestha" age = {100} isStudent = {true} />
+      <Student name="Priya Bajracharya" age = {99} isStudent = {true} />
+      <Student name="Garima Shrestha" age = {98} isStudent = {true} />
+    </>
+  )
+}
+
+export default App
